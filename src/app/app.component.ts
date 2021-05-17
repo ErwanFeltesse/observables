12fbc73dbfc956adae421ffb44b7fd83bdac476e
ErwanFeltesse <er.feltesse@gmail.com>
2021-05-17 15:41:11 +0200

    lesson done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'observable';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const subject = new Subject<number>();
+
+    const s1 = subject.subscribe((x) => {
+      console.log('[s1] : ', x);
+    });
+    const s2 = subject.subscribe((x) => {
+      console.log('[s2] : ', x);
+    });
+
+    subject.next(1);
+    s1.unsubscribe();
+    subject.next(2);
+  }
 }
